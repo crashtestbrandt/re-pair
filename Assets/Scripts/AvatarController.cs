303e18c8dbc723285e2b04a5e9a105ae34e84f82
Brandt <starshipbrandt@gmail.com>
2020-02-02 05:30:51 -0600

    Now with more players

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -7,8 +7,8 @@ public class AvatarController : MonoBehaviour
 {
     public int MAX_JUMPS = 2;
     public float AIR_CONTROL_FACTOR = 0.5f;
-    public float BASE_MOVEMENT_SPEED = 10f;
-    public float BASE_JUMP_SPEED = 20f;
+    public float BASE_MOVEMENT_SPEED = 3f;
+    public float BASE_JUMP_SPEED = 4f;
 
     public SpriteRenderer sprite;
     private Rigidbody2D rb;
@@ -28,25 +28,17 @@ public class AvatarController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        var gamepad = Gamepad.current;
-        if (gamepad == null) return;
         rb.velocity = new Vector2(
             BASE_MOVEMENT_SPEED * xInput,
             rb.velocity.y
         );
-
-        if (!grounded) {
-            //jumpStartVelocity = rb.velocity;
-        }
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("Player upward velocity: " + rb.velocity.y.ToString());
-        
+    private void OnCollisionEnter2D(Collision2D other) {        
         if (other.gameObject.CompareTag("Floor")) {
             if (!grounded) {
                 grounded = true;
-                jumpStartVelocity = Vector2.zero;
+                rb.velocity = Vector2.zero;
                 RestoreJumps();
             }
         }
@@ -54,27 +46,21 @@ public class AvatarController : MonoBehaviour
 
     public void OnMove(InputValue inputVal) {
         xInput = grounded? inputVal.Get<Vector2>().x : AIR_CONTROL_FACTOR * inputVal.Get<Vector2>().x;
-        if (xInput < 0) sprite.flipX = false;
-        else if (xInput > 0) sprite.flipX = true;
+        if (xInput < 0) sprite.flipX = true;
+        else if (xInput > 0) sprite.flipX = false;
         // else x := 0
     }
 
-    public void OnGamepadButtonDown(InputValue inputVal) {
-        var gamepad = Gamepad.current;
-        if (gamepad.aButton.wasPressedThisFrame) {
-            if (numJumps > 0) {
-                if (buddyJumpEnabled) buddyJumpEnabled = false;
-                else numJumps--;
+    public void OnJump() {
+        if (numJumps > 0) {
+            if (buddyJumpEnabled) buddyJumpEnabled = false;
+            else numJumps--;
 
-                rb.velocity = new Vector2(rb.velocity.x, BASE_JUMP_SPEED);
-            }
+            rb.velocity = new Vector2(rb.velocity.x, BASE_JUMP_SPEED);
+        }
 
-            if (grounded) {
-                grounded = false;
-                jumpStartVelocity = rb.velocity;
-            }
-
-
+        if (grounded) {
+            grounded = false;
         }
     }
 
